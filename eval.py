efc14b2d4ceb116bd6dc2ae8f10048a2505df51f
Liskie <wyj125869@126.com>
2022-04-25 16:49:30 +0800

    Modified eval.py for counting.

--- eval.py
+++ eval.py
@@ -3,6 +3,10 @@ from sklearn.metrics import f1_score, precision_score, recall_score
 def main():
     tags_true = []
     tags_pred = []
+
+    correct_count = 0
+    valid_line_count = 0
+
     for line in open("output.txt"):
         line = line.strip()
         if not line:
@@ -10,6 +14,13 @@ def main():
         _, tag_true, tag_pred = line.split()
         tags_true.append(tag_true)
         tags_pred.append(tag_pred)
+
+        valid_line_count += 1
+        if tag_true == tag_pred:
+            correct_count += 1
+
+    print(f'{correct_count} out of {valid_line_count} predictions are correct.')
+
     print(f'Precision: {precision_score(tags_true, tags_pred, average="macro")}\n'
           f'Recall: {recall_score(tags_true, tags_pred, average="macro")}\n'
           f'F1: {f1_score(tags_true, tags_pred, average="macro")}')
